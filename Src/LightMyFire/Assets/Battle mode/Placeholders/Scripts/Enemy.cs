f08534aac8fdba479277f6b52d2e66c47a2bee49
Vojtěch Tázlar <tazlarvojta@centrum.cz>
2018-11-27 00:01:25 +0100

    Rat boss fight + player controller + player animations + shooting logic

---
+++ Src/LightMyFire/Assets/Battle mode/Placeholders/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] private int health = 100;
+
+	public void TakeDamage(int damage) {
+		health -= damage;
+		if (health <= 0) { Destroy(gameObject); }
+	}
+}
---
+++ Src/LightMyFire/Assets/Battle mode/Scripts/AnimationAutoDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAutoDestroy : MonoBehaviour {
+
+	[SerializeField] private float delay = 0f;
+
+	void Start() {
+		Destroy(gameObject, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+	}
+}
+
