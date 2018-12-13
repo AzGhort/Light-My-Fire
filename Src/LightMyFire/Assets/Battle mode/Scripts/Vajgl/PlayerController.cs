d625d734c4742c012069e3f64f2b937f0337359d
Vojtěch Tázlar <tazlarvojta@centrum.cz>
2018-12-13 12:49:14 +0100

    Player animation setup + Player scripts rewrite

---
+++ Src/LightMyFire/Assets/Battle mode/Scripts/Vajgl/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LightMyFire
+{
+	[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField] private float runSpeed = 40f;                              // Maximum speed
+		[SerializeField] [Range(0, .3f)] private float movementSmoothing = .05f;    // How much to smooth out the movement
+
+		[SerializeField] private float jumpForce = 700f;            // Amount of force added when the player jumps.
+		[SerializeField] private bool airControl = true;            // Whether or not a player can steer while jumping;
+
+		[SerializeField] private LayerMask groundMask;              // A mask determining what is ground to the character
+		[SerializeField] private Transform groundCheck;             // A position marking where to check if the player is grounded
+		[SerializeField] private float groundedCheckRadius = .2f;   // Radius of the overlap circle to determine if grounded
+
+		// Necessary components of player game object
+		private Rigidbody2D rigidbody2d;
+		private Animator animator;
+
+		private Collider2D[] groundCheckColliders = new Collider2D[1];
+		private Vector2 velocity = Vector2.zero;
+
+		// State variables
+		private bool isGrounded = false;
+		private bool isFacingRight = true;
+
+		// Input
+		private float horizontalMovement = 0f;
+		private bool jump = false;
+
+		private void Awake() {
+			rigidbody2d = GetComponent<Rigidbody2D>();
+			animator = GetComponent<Animator>();
+
+			Debug.Assert(rigidbody2d);
+			Debug.Assert(animator);
+		}
+
+		private void Update() {
+			if (PauseMenu.GameIsPaused) { return; }
+
+			horizontalMovement = Input.GetAxisRaw("Horizontal");
+			if (Input.GetButtonDown("Jump")) { jump = true; }
+			//jump = Input.GetButtonDown("Jump") ? true : false;
+		}
+
+		private void FixedUpdate() {
+			checkGround();
+			move();
+		}
+
+		private void checkGround() {
+			bool wasGrounded = isGrounded;
+			isGrounded = false;
+
+			// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+			int groundCollisions = Physics2D.OverlapCircleNonAlloc(groundCheck.position, groundedCheckRadius, groundCheckColliders, groundMask);
+			if (groundCollisions > 0) {
+				isGrounded = true;
+				if (!wasGrounded) {
+					jump = false;
+					animator.SetBool("IsJumping", false);
+				}
+			}
+		}
+
+		private void move() {
+			// Movement control
+
+			// Move the character by finding the target velocity
+			float xTargetVelocity;
+			if (horizontalMovement == 0f && isGrounded) {
+				// Stop movement in x axis
+				xTargetVelocity = 0f;
+			}
+			else {
+				xTargetVelocity = horizontalMovement * Time.fixedDeltaTime * runSpeed * 10f;
+			}
+			Vector2 targetVelocity = new Vector2(xTargetVelocity, rigidbody2d.velocity.y);
+
+			// Smoothing out movement and apply it to the character
+			rigidbody2d.velocity = Vector2.SmoothDamp(rigidbody2d.velocity, targetVelocity, ref velocity, movementSmoothing);
+			animator.SetFloat("Speed", Mathf.Abs(rigidbody2d.velocity.x));
+
+			// Flip player if movement direction is different than current orientation
+			if (isGrounded || airControl) {
+				if ((xTargetVelocity > 0 && !isFacingRight) || (xTargetVelocity < 0 && isFacingRight)) { Flip(); }
+			}
+
+			//Add a vertical force to the player => jump
+			if (isGrounded && jump) {
+				animator.SetBool("IsJumping", true);
+				rigidbody2d.AddForce(new Vector2(0f, jumpForce));
+				isGrounded = false;
+				jump = false;
+			}
+		}
+
+		// Flips player orientation by rotation
+		private void Flip() {
+			transform.Rotate(0f, 180f, 0f);
+			isFacingRight = !isFacingRight;
+		}
+	}
+}
---
+++ Src/LightMyFire/Assets/Battle mode/Scripts/Vajgl/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace LightMyFire
+{
+	[RequireComponent(typeof(Animator))]
+	public class PlayerShoot : MonoBehaviour
+	{
+		[SerializeField] private GameObject projectilePrefab;
+		[SerializeField] private Transform firePoint;
+
+		private Animator animator;
+
+		public void FireProjectile() {
+			Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+		}
+
+		public void ReloadProjectile() {
+			animator.SetBool("Fire", false);
+		}
+
+		private void Awake() {
+			animator = GetComponent<Animator>();
+			Debug.Assert(animator);
+		}
+
+		private void Update() {
+			if (PauseMenu.GameIsPaused) { return; }
+			if (Input.GetButtonDown("Fire1")) {
+				animator.SetBool("Fire", true);
+			}
+		}
+	}
+}
